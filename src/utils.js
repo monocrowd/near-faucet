13d2cae809ebe86cd8bde4fec6def8c3169762bf
monocrowd <monocrowd@gmail.com>
2021-12-01 21:17:15 +0800

    add airdrop

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
-import { connect, Contract, keyStores, WalletConnection } from 'near-api-js'
+import { connect, KeyPair, keyStores, WalletConnection } from 'near-api-js'
 import getConfig from './config'
 
 const nearConfig = getConfig(process.env.NODE_ENV || 'development')
@@ -16,13 +16,48 @@ export async function initContract() {
   window.accountId = window.walletConnection.getAccountId()
 
   // Initializing our contract APIs by contract name and configuration
-  window.contract = await new Contract(window.walletConnection.account(), nearConfig.contractName, {
-    // View methods are read only. They don't modify the state, but usually return some value.
-    viewMethods: ['get_greeting'],
-    // Change methods can modify the state. But you don't receive the returned value when called.
-    changeMethods: ['set_greeting'],
-  })
+  // window.contract = await new Contract(window.walletConnection.account(), nearConfig.contractName, {
+  //   // View methods are read only. They don't modify the state, but usually return some value.
+  //   viewMethods: ['get_greeting'],
+  //   // Change methods can modify the state. But you don't receive the returned value when called.
+  //   changeMethods: ['set_greeting'],
+  // })
 }
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+export async function airdrop (toAccountId, amount) {
+  console.log("request " + amount)
+  const keyStore = new keyStores.InMemoryKeyStore()
+  const near = await connect(Object.assign({ deps: { keyStore: keyStore } }, nearConfig))  
+
+  const count = amount / 200;
+  for(var i=0; i<count; i++) {
+    // create dev account and delete
+    const randomNumber = Math.floor(Math.random() * (99999999999999 - 10000000000000) + 10000000000000);
+    accountId = `dev-${Date.now()}-${randomNumber}`;
+    const keyPair = await KeyPair.fromRandom('ed25519');
+
+    const account = await near.createAccount(accountId, keyPair.publicKey);
+    await keyStore.setKey(nearConfig.networkId, account.accountId, keyPair);
+
+    await sleep(5000);
+    console.log("created temp account " + accountId)
+    
+    while (true) {
+      try {
+        await account.deleteAccount(toAccountId); 
+        break;
+      } catch (error) {}
+    }
+    console.log("deleted temp account " + accountId)
+  }
+  
+  window.accountId = window.walletConnection.getAccountId()
+}
+
 
 export function logout() {
   window.walletConnection.signOut()
